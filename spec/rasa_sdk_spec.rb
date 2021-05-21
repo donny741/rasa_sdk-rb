4188c4045cb60ed055a9dfdeaefc95bcac506a83
Donatas Povilaitis <ddonatasjar@gmail.com>
2021-05-21 15:01:22 +0300

    Fix gemspec

--- spec/rasa_sdk_spec.rb
+++ spec/rasa_sdk_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RasaSDK do
   it "has a version number" do
     expect(RasaSDK::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
